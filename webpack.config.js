8aabc619cd8f07e095cc3b406395f1c81ecef729
David Díaz <diazdavid.info@gmail.com>
2017-04-02 13:02:19 +0200

    Kata de less con sass

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+let path = require('path');
+let ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    entry: {
+        app: ['./src/index.jsx', './src/main.scss']
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'public'),
+        publicPath: '/'
+    },
+    module: {
+        rules: [
+            {
+                test: /(\.js|.jsx)$/,
+                loader: 'babel-loader',
+                exclude: /node_module/
+            },
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader', 'sass-loader']
+                })
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin('style.css')
+    ],
+    devServer: {
+        host: '0.0.0.0',
+        port: 8000,
+        inline: true,
+        historyApiFallback: true
+    }
+};
